76c0697ba08cf417c187165717171de6b26a43c1
Lucas Limeira Souza <lucas.limeira@soulisto.com.br>
2024-02-24 10:45:14 -0300

    fix commit

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit_simple/app/pages/home/cubit/todo_cubit.dart';
-import 'package:flutter_cubit_simple/app/pages/home_page.dart';
+import 'package:flutter_cubit_simple/app/pages/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
